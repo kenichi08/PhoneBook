16fce1929b6770786d8e6f82576b9ada7130f15a
kenichi08 <43532574+kenichi08@users.noreply.github.com>
2018-09-24 14:52:57 +0800

    Add files via upload

---
+++ phonebook/js/routes.js
@@ -0,0 +1,27 @@
+angular.module('app.routes', [])
+
+.config(function($stateProvider, $urlRouterProvider) {
+
+  // Ionic uses AngularUI Router which uses the concept of states
+  // Learn more here: https://github.com/angular-ui/ui-router
+  // Set up the various states which the app can be in.
+  // Each state's controller can be found in controllers.js
+  $stateProvider
+    
+
+      .state('cONTACTS', {
+    url: '/page1',
+    templateUrl: 'templates/cONTACTS.html',
+    controller: 'cONTACTSCtrl'
+  })
+
+  .state('page2', {
+    url: '/page2',
+    templateUrl: 'templates/page2.html',
+    controller: 'page2Ctrl'
+  })
+
+$urlRouterProvider.otherwise('/page1')
+
+
+});
